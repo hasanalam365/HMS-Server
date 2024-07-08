db628f7756044f24a167fc4f7cb9847d6c824187
hasanalam24 <hasanalam365@gmail.com>
2024-07-08 06:47:01 +0600

    db connect and problem solve db bad auth

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
-require('dotenv').config()
-const port = process.env.PORT || 5000
-
-
-app.use(cors())
-app.use(express.json())
-
-
-console.log(process.env.MONGO_PASS)
+// const jwt = require('jsonwebtoken')
+// const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://hmsshop24:<password>@cluster0.ruz3wge.mongodb.net/?appName=Cluster0";
+// const axios = require('axios');
+require('dotenv').config();
+const app = express()
+
+const port = process.env.PORT || 5000;
+
+
+
+app.use(express.json())
+app.use(cors())
+
+
+
+
+// const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.ruz3wge.mongodb.net/hmsshop`;
+
+// console.log(process.env.MONGO_USER, process.env.MONGO_PASS)
+
+
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.qvnsypp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,19 +37,24 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+
+        const productCollection = client.db('ShopDB').collection('products');
+
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
-
-
-
-
 
 
 app.get('/', async (req, res) => {
